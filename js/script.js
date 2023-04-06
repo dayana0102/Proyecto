5bbe457ab9b566cf571a136ef4d5fc53a400a6e4
dyn <dayana26ce@gmail.com>
2023-04-06 15:12:44 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+/*
+JavaScrip - PROYECTO - DAMYLYER
+*/
+
+$(window).load(function() { 
+	//Preloader 
+	$('#status').delay(300).fadeOut(); 
+	$('#preloader').delay(300).fadeOut('slow');
+	$('body').delay(550).css({'overflow':'visible'});
+})
+
+$(document).ready(function() {
+		//logo animado
+		$(".navbar-brand").hover(function () {
+			$(this).toggleClass("animated shake");
+		});
+		
+		//desplazamiento animado
+		$(".img_scroll").hover(function () {
+			$(this).toggleClass("animated infinite bounce");
+		});
+		
+		//Animacion en el Movil
+		wow = new WOW(
+		{
+			mobile: false
+		});
+		wow.init();
+		
+		//MagnificPopup
+		$('.image-link').magnificPopup({type:'image'});
+
+
+		// OwlCarousel N1
+		$("#owl-demo").owlCarousel({
+			autoPlay: 3000,
+			items : 3,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,3]
+		});
+
+		// OwlCarousel N2
+		$("#owl-demo-1").owlCarousel({
+			  navigation : false, // Mostrar los botones siguiente y anterior
+			  slideSpeed : 300,
+			  paginationSpeed : 400,
+			  singleItem:true
+		});
+
+		//Desplazamiento suave
+		$('a[href*=#]').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+					var $target = $(this.hash);
+					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+					if ($target.length) {
+							var targetOffset = $target.offset().top;
+							$('html,body').animate({scrollTop: targetOffset}, 600);
+							return false;
+					}
+			}
+		});
+		
+		//Subscribe
+		new UIMorphingButton( document.querySelector( '.morph-button' ) );
+		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
+		} );
+
+});
+
